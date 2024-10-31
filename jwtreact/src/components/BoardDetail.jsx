79b3a091b0e669d934981b45de30aba4deae8c71
Ssong0515 <406605@naver.com>
2024-10-31 14:42:49 +0900

    학원 마지막

---
+++ jwtreact/src/components/BoardDetail.jsx
@@ -0,0 +1,21 @@
+import {apiBoardDetail} from "../service/ApiService.js";
+
+function BoardDetail() {
+  return (
+    <div className={'container mt-5 p-5'}>
+      {apiBoardDetail(100)}
+    </div>
+  );
+}
+
+export default BoardDetail
+
+
+
+
+
+
+
+
+
+
---
+++ jwtreact/src/components/layout/Footer.jsx
@@ -0,0 +1,23 @@
+
+
+function Footer() {
+  return (
+    <div>
+      <footer className={'container-fluid border-top mt-5 p-5'}>
+        <p className={'lead text-muted text-center'}>made by fullstack405</p>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer
+
+
+
+
+
+
+
+
+
+
